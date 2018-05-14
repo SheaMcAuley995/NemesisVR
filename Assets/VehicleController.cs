75e56b7c122b0617bd79915f97c476d6345d59c7
Shea McAuley <s179049@students.aie.edu.au>
2018-05-14 13:56:28 -0700

    merging over to shea

---
+++ Assets/VehicleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour {
+
+    public float acceleration;
+    public float turnSpeed;
+
+    public float turnRotationAngle;
+    public float turnRotationSeekSpeed;
+
+    private float rotationVelocity;
+    private float groundAngelVelocity;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        if(Physics.Raycast (transform.position, transform.up * -1, 3f))
+        {
+            rb.drag = 1;
+
+            Vector3 forwardForce = transform.right * acceleration * Input.GetAxis("Vertical");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
