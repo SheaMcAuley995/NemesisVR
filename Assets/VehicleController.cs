038d72a9dab72f2028c926dfba18b7d8bfad61e1
Shea McAuley <s179049@students.aie.edu.au>
2018-05-14 16:41:51 -0700

    Shea worked in artdev

--- Assets/VehicleController.cs
+++ Assets/VehicleController.cs
@@ -26,7 +26,24 @@ public class VehicleController : MonoBehaviour {
             rb.drag = 1;
 
             Vector3 forwardForce = transform.right * acceleration * Input.GetAxis("Vertical");
+
+            forwardForce = forwardForce * Time.deltaTime * rb.mass;
+
+            rb.AddForce(forwardForce);
         }
+        else
+        {
+            rb.drag = 0;
+        }
+
+        Vector3 turnTorque = Vector3.up * turnSpeed * Input.GetAxis("Horizontal");
+
+        turnTorque = turnTorque * Time.deltaTime * rb.mass;
+        rb.AddTorque(turnTorque);
+
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, Input.GetAxis("Horizontal") * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
 
     }
 
