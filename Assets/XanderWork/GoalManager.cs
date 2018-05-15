d2f0218d0b8a5ea42b2460be34f877e2e86e70b0
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-15 11:06:55 -0700

    Add goal scripts & prefabs

---
+++ Assets/XanderWork/GoalManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    public int scorePerGoal = 1;
+
+    public GoalTrigger sunGoal;
+    public GoalTrigger moonGoal;
+
+
+
+    private void Awake()
+    {
+        sunGoal.goalTriggered += SunGoalTrigger;
+        moonGoal.goalTriggered += MoonGoalTrigger;
+    }
+
+    public void SunGoalTrigger(Collider other)
+    {
+        if(other.tag == "Ball")
+        {
+            ScoreManager.Instance.AddScoreSun(scorePerGoal);
+        }
+    }
+
+    public void MoonGoalTrigger(Collider other)
+    {
+        if (other.tag == "Ball")
+        {
+            ScoreManager.Instance.AddScoreMoon(scorePerGoal);
+        }
+    }
+
+}
---
+++ Assets/XanderWork/GoalTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour {
+
+    public delegate void GoalTriggered(Collider other);
+    public GoalTriggered goalTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(goalTriggered != null)
+        {
+            goalTriggered(other);
+        }
+    }
+
+}
