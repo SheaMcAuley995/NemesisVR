d0d1923790e9afd7c5cd70ec6caf987a70888ee5
Shea McAuley <s179049@students.aie.edu.au>
2018-05-15 10:13:30 -0700

    ball singleton

--- Assets/HoverBall.cs
+++ Assets/HoverBall.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class HoverBall : MonoBehaviour {
 
+#region singleton
+
+    private static HoverBall instance;
+    public static HoverBall Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    //private void Awake()
+    //{
+    //    instance = this;
+    //}
+
+#endregion
+
     public float hoverDistance;
     public float hoverStrength;
+
+    public LayerMask groundLayerMask;
+
+    public Transform resetPos;
+
+    public delegate void MyHoverBallDelegate();
+    public MyHoverBallDelegate myHoverBallDelegate;
 
 
     Rigidbody rb;
@@ -19,11 +44,16 @@ public class HoverBall : MonoBehaviour {
         Vector3 downwardForce;
         float distancePercentage;
 
-        if (Physics.Raycast(transform.position, -transform.up, out hit, hoverDistance))
+        if (Physics.Raycast(transform.position, -transform.up, out hit, hoverDistance, groundLayerMask))
         {
             distancePercentage = 1 - (hit.distance / hoverDistance);
             downwardForce = (transform.up * hoverStrength * distancePercentage) * Time.deltaTime;
             rb.AddForce(downwardForce);
         }
+    }
+
+    public void ballReset()
+    {
+        transform.position = resetPos.position;
     }
 }
