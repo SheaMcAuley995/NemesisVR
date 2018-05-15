8c370f3d94c6f046e83326e8124aa45be7f7f4b7
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-15 10:08:44 -0700

    Add XanderScene and ScoreManager script

---
+++ Assets/XanderWork/ScoreManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    private ScoreManager instance;
+    public ScoreManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private int scoreSun = 0;
+    public int ScoreSun
+    {
+        get
+        {
+            return scoreSun;
+        }
+    }
+
+    private int scoreMoon = 0;
+    public int ScoreMoon
+    {
+        get
+        {
+            return scoreMoon;
+        }
+    }
+
+    public int scoreToWin;
+
+    public delegate void OnScoreIncrease(int score);
+    public OnScoreIncrease onScoreSunIncrease;
+    public OnScoreIncrease onScoreMoonIncrease;
+
+
+
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddScoreSun(int amt)
+    {
+        scoreSun += amt;
+        if(onScoreSunIncrease != null)
+        {
+            onScoreSunIncrease(scoreSun);
+        }
+    }
+
+    public void AddScoreMoon(int amt)
+    {
+        scoreMoon += amt;
+        if (onScoreMoonIncrease != null)
+        {
+            onScoreMoonIncrease(scoreMoon);
+        }
+    }
+
+}
