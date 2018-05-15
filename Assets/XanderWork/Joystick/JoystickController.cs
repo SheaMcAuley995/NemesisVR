bb03d793ae84693c915de0531c452ec5ccbeb33e
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-15 15:07:12 -0700

    Add empty joystick scripts

---
+++ Assets/XanderWork/Joystick/JoystickController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickController : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        Debug.Log(transform.eulerAngles);
+    }
+
+    private void Update()
+    {
+        //Rotate(10.0f * Time.deltaTime, 10.0f * Time.deltaTime);
+        //Rotate(0, 10.0f * Time.deltaTime);
+        //Rotate(10.0f * Time.deltaTime, 0);
+    }
+
+    public void Rotate(float roll, float pitch)
+    {
+        transform.eulerAngles += Vector3.forward * roll;
+        transform.eulerAngles += Vector3.right * pitch;
+    }
+
+}
---
+++ Assets/XanderWork/Joystick/JoystickHandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickHandle : MonoBehaviour {
+
+    public Renderer renderer;
+    public Material normalMat;
+    public Material touchingMat;
+    public Material holdingMat;
+
+
+
+    private void Start()
+    {
+        renderer.material = normalMat;
+    }
+
+
+
+}
