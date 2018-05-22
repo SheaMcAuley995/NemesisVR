23d96d1bb04ca03da4ec113d8a691c741dacab5d
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-22 14:53:13 -0700

    Add goal posts

--- Assets/XanderWork/ScoreAndGoals/GoalManager.cs
+++ Assets/XanderWork/ScoreAndGoals/GoalManager.cs
@@ -9,12 +9,45 @@ public class GoalManager : MonoBehaviour {
     public GoalTrigger sunGoal;
     public GoalTrigger moonGoal;
 
+    public Transform sunPost;
+    public Transform moonPost;
+    public Transform sunPostEnd;
+    public Transform moonPostEnd;
+
+    public float postMoveSpeed;
+
+    private Vector3 sunPostMoveTo;
+    private Vector3 moonPostMoveTo;
+    private Vector3 sunPostMoveIncrement;
+    private Vector3 moonPostMoveIncrement;
+
 
 
     private void Awake()
     {
         sunGoal.goalTriggered += SunGoalTrigger;
         moonGoal.goalTriggered += MoonGoalTrigger;
+        sunPostMoveTo = sunPost.position;
+        moonPostMoveTo = moonPost.position;
+
+        sunPostMoveIncrement = (sunPostEnd.position - sunPost.position).normalized *
+                                  (Vector3.Distance(sunPost.position, sunPostEnd.position)
+                                   / ScoreManager.Instance.scoreToWin);
+        moonPostMoveIncrement = (moonPostEnd.position - moonPost.position).normalized *
+                                  (Vector3.Distance(moonPost.position, moonPostEnd.position)
+                                   / ScoreManager.Instance.scoreToWin);
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(sunPost.position, sunPostMoveTo) > postMoveSpeed)
+        {
+            sunPost.position += (sunPostMoveTo - sunPost.position).normalized * postMoveSpeed * Time.deltaTime;
+        }
+        if (Vector3.Distance(moonPost.position, moonPostMoveTo) > postMoveSpeed)
+        {
+            moonPost.position += (moonPostMoveTo - moonPost.position).normalized * postMoveSpeed * Time.deltaTime;
+        }
     }
 
     public void SunGoalTrigger(Collider other)
@@ -22,6 +55,10 @@ public class GoalManager : MonoBehaviour {
         if(other.tag == "Ball")
         {
             HoverBall.Instance.ballReset();
+            if(ScoreManager.Instance.ScoreSun < ScoreManager.Instance.scoreToWin)
+            {
+                sunPostMoveTo += sunPostMoveIncrement;
+            }
             ScoreManager.Instance.AddScoreSun(scorePerGoal);
         }
     }
@@ -31,6 +68,10 @@ public class GoalManager : MonoBehaviour {
         if (other.tag == "Ball")
         {
             HoverBall.Instance.ballReset();
+            if (ScoreManager.Instance.ScoreMoon < ScoreManager.Instance.scoreToWin)
+            {
+                moonPostMoveTo += moonPostMoveIncrement;
+            }
             ScoreManager.Instance.AddScoreMoon(scorePerGoal);
         }
     }
