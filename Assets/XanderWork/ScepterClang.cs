fef7a539c45a76837f041a1f2375d5f7a6124f39
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-23 13:12:00 -0700

    Implement scepters clanging when they hit each other & beetle

---
+++ Assets/XanderWork/ScepterClang.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScepterClang : MonoBehaviour {
+
+    public float volume;
+    public float speedThreshold;
+    public AudioSource audio;
+    public Transform headPos;
+
+    private Vector3 lastHeadPos;
+    private Vector3 headOffset = Vector3.zero;
+    private Vector3 correctLocalPos;
+    private Vector3 correctLocalEuler;
+
+
+
+    private void Start()
+    {
+        lastHeadPos = headPos.position;
+        correctLocalPos = transform.localPosition;
+        correctLocalEuler = transform.localEulerAngles;
+    }
+
+    private void Update()
+    {
+        transform.localPosition = correctLocalPos;
+        transform.localEulerAngles = correctLocalEuler;
+        headOffset = headPos.position - lastHeadPos;
+        lastHeadPos = headPos.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(headOffset.magnitude >= speedThreshold)
+        {
+            audio.PlayOneShot(audio.clip, headOffset.magnitude * volume);
+        }
+    }
+
+}
