e5e1697d995aa4ca613160346ef3ea6a2d95ffdd
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-29 16:46:36 -0700

    Add spell pickup & shoot

---
+++ Assets/XanderWork/Spellcasting/SpellCaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class SpellCaster : MonoBehaviour {
+
+        public float spellShootForce;
+        public Hand hand;
+        public GameObject spellPrefab = null;
+        public Transform spellEffectObj = null;
+        public bool spellShootDebounce = true;
+
+        private void LateUpdate()
+        {
+            if(spellEffectObj != null)
+            {
+                spellEffectObj.position = transform.position;
+                spellEffectObj.rotation = transform.rotation;
+                if(hand.GetStandardInteractionButtonDown() && !spellShootDebounce)
+                {
+                    GameObject spell = Instantiate(spellPrefab);
+                    spell.transform.position = transform.position;
+                    spell.transform.rotation = transform.rotation;
+                    spell.GetComponent<Rigidbody>().AddForce(spellEffectObj.forward * spellShootForce);
+                    spellPrefab = null;
+                    Destroy(spellEffectObj.gameObject);
+                    spellEffectObj = null;
+                }
+                spellShootDebounce = false;
+            }
+        }
+
+    }
+}
---
+++ Assets/XanderWork/Spellcasting/SpellZone.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Valve.VR.InteractionSystem
+{
+    public class SpellZone : MonoBehaviour
+    {
+
+        public GameObject spellPrefab;
+        public GameObject staffSpellEffectPrefab;
+
+        [Header("PROTOTYPING")]
+        public Renderer renderer;
+        public Material normalMat;
+        public Material touchingMat;
+
+        private int touchingHandCount = 0;
+        private List<Hand> handsIn = new List<Hand>();
+        private List<SpellCaster> handHeads = new List<SpellCaster>();
+
+
+        private void Start()
+        {
+            renderer.material = normalMat;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            renderer.material = touchingMat;
+            handsIn.Add(other.transform.parent.parent.GetComponent<Hand>());
+            handHeads.Add(other.transform.GetComponent<SpellCaster>());
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            handsIn.Remove(other.transform.parent.parent.GetComponent<Hand>());
+            handHeads.Remove(other.transform.GetComponent<SpellCaster>());
+            if (handsIn.Count == 0)
+            {
+                renderer.material = normalMat;
+            }
+        }
+
+        private void Update()
+        {
+            for(int i = 0; i < handsIn.Count; ++i)
+            {
+                Hand hand = handsIn[i];
+                if(hand.GetStandardInteractionButtonDown() && handHeads[i].spellEffectObj == null)
+                {
+                    GameObject spellEffect = Instantiate(staffSpellEffectPrefab);
+                    spellEffect.transform.position = handHeads[i].transform.position;
+                    spellEffect.transform.rotation = handHeads[i].transform.rotation;
+                    handHeads[i].spellEffectObj = spellEffect.transform;
+                    handHeads[i].spellPrefab = spellPrefab;
+                    handHeads[i].spellShootDebounce = true;
+                    //handsIn.RemoveAt(i);
+                    //handHeads.RemoveAt(i);
+                    //--i;
+                }
+            }
+        }
+
+    }
+}
