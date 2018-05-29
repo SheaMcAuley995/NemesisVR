47897cdf9f9d49081b58cfb06da8605fd678e529
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-29 11:52:29 -0700

    Add speed zones

---
+++ Assets/XanderWork/SpeedZone/SpeedZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedZone : MonoBehaviour
+{
+
+    public float force;
+
+    private List<Rigidbody> bodies = new List<Rigidbody>();
+
+
+
+    private void FixedUpdate()
+    {
+        foreach(Rigidbody b in bodies)
+        {
+            b.AddForce(transform.forward * force);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody b = other.GetComponent<Rigidbody>();
+        if(b != null)
+        {
+            bodies.Add(b);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody b = other.GetComponent<Rigidbody>();
+        if (b != null)
+        {
+            bodies.Remove(b);
+        }
+    }
+
+}
