973f8cb2b40c1af439f8eb319f2b35c21b09f9f9
Shea McAuley <s179049@students.aie.edu.au>
2018-05-30 10:30:10 -0700

    Team manager

--- Assets/Scripts/AI/TeamManager.cs
+++ Assets/Scripts/AI/TeamManager.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class TeamManager : MonoBehaviour {
 
-    //public int capacity;
+    public int capacity;
 
     public List<GameObject> TeamSun;
+    private bool hasBall = false;
     public List<GameObject> TeamMoon;
 
-    private void Awake()
-    {
-        //TeamSun.Capacity = capacity;
-    }
 }
