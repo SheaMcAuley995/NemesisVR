83c6a6413f261bc76939d086b53c730f21d412c2
Shea McAuley <s179049@students.aie.edu.au>
2018-05-29 17:55:35 -0700

    For some reason didn't save team manager

--- Assets/Scripts/AI/TeamManager.cs
+++ Assets/Scripts/AI/TeamManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TeamManager : MonoBehaviour {
 
-    public int capacity;
+    //public int capacity;
 
     public List<GameObject> TeamSun;
     public List<GameObject> TeamMoon;
 
     private void Awake()
     {
-        TeamSun.Capacity = capacity;
+        //TeamSun.Capacity = capacity;
     }
 }
