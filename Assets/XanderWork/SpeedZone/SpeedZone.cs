39b1c23e4cfe09c9fc0bd37cdb2e0958e3447a4f
Alexander Thomas <s179069@students.aie.edu.au>
2018-06-04 11:42:16 -0700

    Make speed zones just reduce drag

--- Assets/XanderWork/SpeedZone/SpeedZone.cs
+++ Assets/XanderWork/SpeedZone/SpeedZone.cs
@@ -13,18 +13,19 @@ public class SpeedZone : MonoBehaviour
 
     private void FixedUpdate()
     {
-        foreach(Rigidbody b in bodies)
-        {
-            float angle = Vector3.Angle(b.velocity.normalized, transform.forward);
-            if(angle < 90)
-            {
-                b.AddForce(transform.forward * force);
-            }
-            else
-            {
-                b.AddForce(-transform.forward * force);
-            }
-        }
+        //foreach(Rigidbody b in bodies)
+        //{
+        //    b.AddForce(b.transform.forward * force);
+        //    //float angle = Vector3.Angle(b.velocity.normalized, transform.forward);
+        //    //if(angle < 90)
+        //    //{
+        //    //    b.AddForce(transform.forward * force);
+        //    //}
+        //    //else
+        //    //{
+        //    //    b.AddForce(-transform.forward * force);
+        //    //}
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,6 +33,7 @@ public class SpeedZone : MonoBehaviour
         Rigidbody b = other.GetComponent<Rigidbody>();
         if(b != null)
         {
+            b.drag *= 0.25f;
             bodies.Add(b);
         }
     }
@@ -41,6 +43,7 @@ public class SpeedZone : MonoBehaviour
         Rigidbody b = other.GetComponent<Rigidbody>();
         if (b != null)
         {
+            b.drag *= 4.0f;
             bodies.Remove(b);
         }
     }
