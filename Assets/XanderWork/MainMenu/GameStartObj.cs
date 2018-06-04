ee5d9ffe87ebf311ee31fae1c754c3309de7f223
Alexander Thomas <s179069@students.aie.edu.au>
2018-06-04 11:49:37 -0700

    Add way to reach game scene from menu scene

---
+++ Assets/XanderWork/MainMenu/GameStartObj.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Valve.VR.InteractionSystem
+{
+
+    [RequireComponent(typeof(Interactable))]
+    public class GameStartObj : MonoBehaviour
+    {
+
+        public string gameSceneName;
+
+        public Renderer renderer;
+        public Material normalMat;
+        public Material touchingMat;
+        public Material holdingMat;
+
+        private int handsIn = 0;
+
+
+
+        private void Start()
+        {
+            renderer.material = normalMat;
+        }
+
+        private void OnHandHoverBegin(Hand hand)
+        {
+            renderer.material = touchingMat;
+            ++handsIn;
+        }
+
+        private void OnHandHoverEnd(Hand hand)
+        {
+            --handsIn;
+            if(handsIn == 0)
+            {
+                renderer.material = normalMat;
+            }
+        }
+
+        private void HandHoverUpdate(Hand hand)
+        {
+            if (hand.GetStandardInteractionButtonDown())
+            {
+                renderer.material = holdingMat;
+                SceneManager.LoadSceneAsync(gameSceneName);
+            }
+        }
+
+    }
+}
