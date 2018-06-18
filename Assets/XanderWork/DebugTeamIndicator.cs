44a0124ff710bc9568968bf63e42c5cd1b519900
Alexander Thomas <s179069@students.aie.edu.au>
2018-06-18 16:22:08 -0700

    Add debug team indicators to AI beetles

---
+++ Assets/XanderWork/DebugTeamIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTeamIndicator : MonoBehaviour {
+
+    public Material moon;
+    public Material sun;
+
+    public Renderer renderer;
+
+
+
+
+    private void Start()
+    {
+        Invoke("SetMat", 0.02f);
+    }
+
+    private void SetMat()
+    {
+        if (transform.parent.tag == "TeamSun")
+        {
+            renderer.material = sun;
+        }
+        else
+        {
+            renderer.material = moon;
+        }
+    }
+
+}
