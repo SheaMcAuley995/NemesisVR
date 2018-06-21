740f6efb0e568c9f2c9d946926d41e3aae698495
Shea McAuley <s179049@students.aie.edu.au>
2018-06-21 15:57:43 -0700

    Rope finished I think

---
+++ Assets/SheaAssets/Sripts/ActivateRope.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRope : MonoBehaviour {
+
+    public Transform start;
+    public Transform end;
+
+    GameObject ropeNode;
+    public GameObject rope;
+    void Start()
+    {
+            Vector3 destiny = end.position;
+            ropeNode = (GameObject)Instantiate(rope, transform.position, Quaternion.identity);
+            ropeNode.GetComponent<RopePhysics>().destiny = destiny;
+    }
+
+}
