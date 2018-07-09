189b5524cb3cf494e420c1d4a3668c2f5e7b63cd
Alexander Thomas <s179069@students.aie.edu.au>
2018-07-09 10:20:44 -0700

    Reorganize XanderWork and add goal posts & flags

---
+++ Assets/XanderWork/SimpleFlagFlapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFlagFlapper : MonoBehaviour
+{
+    
+    public float maxRot;
+    public float flapSpeed;
+
+    private float rotTo = 0;
+    private int rotDir = 1;
+    private float distanceRotated = 0;
+    private float lastDistFrontCenter = 0;
+
+
+
+    private void Start()
+    {
+        rotTo = transform.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        if(distanceRotated >= rotTo)
+        {
+            rotTo = Random.Range(0, maxRot) + distanceRotated - lastDistFrontCenter;
+            lastDistFrontCenter = distanceRotated - lastDistFrontCenter;
+            distanceRotated = 0;
+
+            if (rotDir == 1)
+            {
+                rotDir = -1;
+            }
+            else
+            {
+                rotDir = 1;
+            }
+        }
+        
+        transform.eulerAngles += Vector3.up * flapSpeed * Time.deltaTime * rotDir;
+        distanceRotated += flapSpeed * Time.deltaTime;
+    }
+
+}
